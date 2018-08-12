f42bb0a8b18b752cae74a1eb2b8b60979aa4effa
Unknown <email.github@textserver.net>
2018-08-12 19:54:54 +0200

    extract efs config in seperate file

--- main.tf
+++ main.tf
@@ -163,58 +163,6 @@ resource "aws_route_table_association" "RT_add_Backend3" {
   route_table_id = "${aws_route_table.RT_Backend.id}"
 }
 
-
-resource "aws_efs_file_system" "efs_dockerStoreDmz" {
-  tags {
-    responsible = "Matthias Malzahn"
-    mm_belong = "${var.tag_mm_belong}"
-       terraform = "true"
-  }
-}
-
-resource "aws_efs_mount_target" "EFS_DMZ1" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreDmz.id}"
-  subnet_id = "${aws_subnet.dmz1.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-resource "aws_efs_mount_target" "EFS_DMZ2" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreDmz.id}"
-  subnet_id = "${aws_subnet.dmz2.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-resource "aws_efs_mount_target" "EFS_DMZ3" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreDmz.id}"
-  subnet_id = "${aws_subnet.dmz3.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-
-
-resource "aws_efs_file_system" "efs_dockerStoreBackend" {
-  tags {
-    responsible = "Matthias Malzahn"
-    mm_belong = "${var.tag_mm_belong}"
-       terraform = "true"
-  }
-}
-
-resource "aws_efs_mount_target" "EFS_Backend1" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreBackend.id}"
-  subnet_id = "${aws_subnet.Backend1.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-resource "aws_efs_mount_target" "EFS_Backend2" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreBackend.id}"
-  subnet_id = "${aws_subnet.Backend2.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-resource "aws_efs_mount_target" "EFS_Backend3" {
-  file_system_id = "${aws_efs_file_system.efs_dockerStoreBackend.id}"
-  subnet_id = "${aws_subnet.Backend3.id}"
-  security_groups = ["${aws_security_group.SG_EFS_IN_FROM_VPC.id}"]
-}
-
-
-
 resource "aws_instance" "nginx_DMZ" {
    ami = "${lookup(var.aws_amis, var.aws_region)}"
    instance_type = "t2.micro"
