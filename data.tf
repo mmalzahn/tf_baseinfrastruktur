b920c4714f5f519c2823bc86796a187742b2c447
Unknown <email.github@textserver.net>
2018-08-12 18:55:08 +0200

    Add internal Domain as Dataresource

--- data.tf
+++ data.tf
@@ -2,6 +2,11 @@ data "aws_route53_zone" "dca_poc_domain" {
   name = "dca-poc.de."
 }
 
+data "aws_route53_zone" "dca_internal_domain" {
+  name = "dca.internal."
+  private_zone = "true"
+}
+
 data "aws_availability_zones" "azs" {
   
 }
