06801616af69e966ff56e2dd31ad5a06761a7bea
Unknown <email.github@textserver.net>
2018-08-12 18:57:21 +0200

    Add Instaces and SG

--- main.tf
+++ main.tf
@@ -216,13 +216,41 @@ resource "aws_efs_mount_target" "EFS_Backend3" {
 
 
 resource "aws_instance" "nginx_DMZ" {
-   ami = "ami-e4515e0e"
+   ami = "${lookup(var.aws_amis, var.aws_region)}"
    instance_type = "t2.micro"
    subnet_id = "${aws_subnet.dmz1.id}"
-   vpc_security_group_ids = ["${aws_security_group.SG_HTTPS_IN.id}"]
+   vpc_security_group_ids = ["${aws_security_group.SG_HTTPS_IN_anywhere.id}","${aws_security_group.SG_SSH_IN_from_Jumphost.id}"]
+   associate_public_ip_address ="true"
+   key_name = "CSA-DemoVPCKey1"
+   user_data = "${file("./installdocker.sh")}"
    tags {
      Name = "DMZ-Proxy"
      responsible = "Matthias Malzahn"
      mm_belong = "${var.tag_mm_belong}"
    }
+}
+
+resource "aws_instance" "jumphost_DMZ" {
+   ami = "${lookup(var.aws_amis, var.aws_region)}"
+   instance_type = "t2.micro"
+   subnet_id = "${aws_subnet.dmz1.id}"
+   vpc_security_group_ids = ["${aws_security_group.SG_SSH_IN_from_anywhere.id}"]
+   key_name = "CSA-DemoVPCKey1"
+   associate_public_ip_address ="true"
+   tags {
+     Name = "DMZ-LinuxJumphost"
+     responsible = "Matthias Malzahn"
+     mm_belong = "${var.tag_mm_belong}"
+       terraform = "true"
+   }
+}
+
+resource "aws_route53_record" "dca_jumphost" {
+  allow_overwrite = "true"
+  depends_on = ["aws_instance.jumphost_DMZ"]
+  name = "jumphost"
+  ttl = "60"
+  type = "A"
+  records =["${aws_instance.jumphost_DMZ.public_ip}"]
+  zone_id = "${data.aws_route53_zone.dca_poc_domain.zone_id}"
 }
--- secgroups.tf
+++ secgroups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "SG_HTTPS_IN" {
+resource "aws_security_group" "SG_HTTPS_IN_anywhere" {
   name        = "SG_HTTPS_IN"
   description = "Allow HTTPS inbound traffic"
   vpc_id      = "${aws_vpc.DemoVPC.id}"
@@ -9,27 +9,24 @@ resource "aws_security_group" "SG_HTTPS_IN" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port       = 0
     to_port         = 65534
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+  
   tags {
     responsible = "Matthias Malzahn"
     mm_belong = "${var.tag_mm_belong}"
        terraform = "true"
   }
-}
-
-resource "aws_security_group_rule" "HTTP_IN" {
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 80
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.SG_HTTPS_IN.id}"
 }
 
 resource "aws_security_group" "SG_EFS_IN_FROM_VPC" {
@@ -53,5 +50,85 @@ resource "aws_security_group" "SG_EFS_IN_FROM_VPC" {
   tags {
     responsible = "Matthias Malzahn"
     mm_belong = "${var.tag_mm_belong}"
+       terraform = "true"
+  }
+}
+
+resource "aws_security_group" "SG_HTTPS_IN_from_Revproxy" {
+  name        = "SG_HTTPS_IN_from_Revproxy"
+  description = "Allow HTTPS inbound traffic from Revproxy"
+  vpc_id      = "${aws_vpc.DemoVPC.id}"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.SG_HTTPS_IN_anywhere.id}"]
+  }
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.SG_HTTPS_IN_anywhere.id}"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 65534
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  tags {
+    responsible = "Matthias Malzahn"
+    mm_belong = "${var.tag_mm_belong}"
+       terraform = "true"
+  }
+}
+resource "aws_security_group" "SG_SSH_IN_from_anywhere" {
+  name        = "SG_SSH_IN_from_anywhere"
+  description = "Allow SSH inbound traffic from anywhere"
+  vpc_id      = "${aws_vpc.DemoVPC.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 65534
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  tags {
+    responsible = "Matthias Malzahn"
+    mm_belong = "${var.tag_mm_belong}"
+    terraform = "true"
+  }
+}
+resource "aws_security_group" "SG_SSH_IN_from_Jumphost" {
+  name        = "SG_SSH_IN_from_Jumphost"
+  description = "Allow SSH inbound traffic from Jumphost"
+  vpc_id      = "${aws_vpc.DemoVPC.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.SG_SSH_IN_from_anywhere.id}"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 65534
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  tags {
+    responsible = "Matthias Malzahn"
+    mm_belong = "${var.tag_mm_belong}"
+    terraform = "true"
   }
 }
