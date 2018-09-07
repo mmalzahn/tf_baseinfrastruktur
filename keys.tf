a9955f2851b7a2ff54edb03bdaa12fd7f24eb594
Unknown <email.github@textserver.net>
2018-09-07 13:28:56 +0200

    update Keyconfig

--- keys.tf
+++ keys.tf
@@ -1,40 +1,48 @@
 resource "tls_private_key" "private_key_bastionhost" {
   count     = "${var.aws_key_name == "" ? 1 : 0}"
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits  = 4096
 }
 
 resource "local_file" "privateKeyFile" {
   count    = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_bastionhost.private_key_pem}"
-  filename = "${path.module}/keys/${terraform.workspace}/private.pem"
+  filename = "${path.module}/keys/${terraform.workspace}/${random_string.dnshostname.result}.key.pem"
 }
 
 resource "local_file" "publicKeyFile" {
   count    = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_bastionhost.public_key_pem}"
-  filename = "${path.module}/keys/${terraform.workspace}/public.pem"
+  filename = "${path.module}/keys/${terraform.workspace}/${random_string.dnshostname.result}.pem"
 }
 
 resource "local_file" "publicKeyFileOpenSsh" {
   count    = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_bastionhost.public_key_openssh}"
-  filename = "${path.module}/keys/${terraform.workspace}/public_openssh.pub"
+  filename = "${path.module}/keys/${terraform.workspace}/${random_string.dnshostname.result}_openssh.pub"
 }
 
-resource "aws_s3_bucket_object" "uploadPubKey" {
-  count      = "${var.aws_key_name == "" ? 1 : 0}"
-  bucket     = "${var.ssh_pubkey_bucket}"
-  content    = "${tls_private_key.private_key_bastionhost.public_key_openssh}"
+resource "aws_s3_bucket_object" "uploadOwnPubkey" {
+  count   = "${var.aws_key_name == "" ? 1 : 0}"
+  bucket  = "${aws_s3_bucket.pubkeyStorageBucket.id}"
+  content = "${tls_private_key.private_key_bastionhost.public_key_openssh}"
+  key     = "keys/${random_string.dnshostname.result}.pub"
   depends_on = ["tls_private_key.private_key_bastionhost"]
-  key        = "${var.ssh_pubkey_prefix}${local.resource_prefix}bastionhost-${random_id.configId.b64_url}.pub"
-  tags       = "${local.common_tags}"
 
   lifecycle {
-    ignore_changes = ["tags.tf_created"]
+    ignore_changes = ["tags"]
   }
+
+  tags = "${local.common_tags}"
 }
 
 data "template_file" "awskeyname" {
   template = "${local.resource_prefix}${lookup(local.common_tags,"tf_project_name")}"
+}
+
+resource "random_string" "dnshostname" {
+  length  = 10
+  special = false
+  upper   = false
+  number  = false
 }
