f8271ba440efdea150bc9ae2375acbb7ddb4208a
Unknown <email.github@textserver.net>
2018-09-10 07:03:57 +0200

    add building of BastionAMI to init-script

--- init.ps1
+++ init.ps1
@@ -8,3 +8,6 @@ terraform.exe init -backend-config backendInit/cfg/backend.cfg
 terraform.exe workspace new prod
 terraform.exe workspace new test
 terraform.exe workspace new dev
+Set-Location -Path "$proPath\packer"
+packer.exe -var-file .\var-bastionhost.json .\bastionhost.json
+Set-Location -Path $proPath
