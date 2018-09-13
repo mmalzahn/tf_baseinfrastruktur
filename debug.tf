9f9157fc5269646841369f7201421913df237b82
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-13 16:13:33 +0200

    add more Debuginfos

--- debug.tf
+++ debug.tf
@@ -21,6 +21,16 @@ resource "local_file" "randomConfigId" {
   content    = "${random_id.configId.b64_url}"
   filename   = "${path.module}/debug/${terraform.workspace}/configId.txt"
 }
+resource "local_file" "projectId" {
+  count      = "${var.debug_on ? 1 : 0 }"
+  content    = "${random_string.projectId.result}"
+  filename   = "${path.module}/debug/${terraform.workspace}/projectId.txt"
+}
+resource "local_file" "dnsName" {
+  count      = "${var.debug_on ? 1 : 0 }"
+  content    = "${random_string.dnshostname.result}"
+  filename   = "${path.module}/debug/${terraform.workspace}/dnsName.txt"
+}
 resource "local_file" "randomPart" {
   count      = "${var.debug_on ? 1 : 0 }"
   content    = "${random_id.randomPart.b64_url}"
@@ -30,4 +40,14 @@ resource "local_file" "bastionAmiId" {
   count      = "${var.debug_on ? 1 : 0 }"
   content    = "${data.aws_ami.bastionhostPackerAmi.id}"
   filename   = "${path.module}/debug/${terraform.workspace}/bastionAmiId.txt"
+}
+resource "local_file" "bastionHostDns" {
+  count      = "${var.debug_on ? 1 : 0 }"
+  content    = "${join(" - ",aws_route53_record.bastionhostdns.*.fqdn)}"
+  filename   = "${path.module}/debug/${terraform.workspace}/bastionHostDns.txt"
+}
+resource "local_file" "bastionHost" {
+  count      = "${var.debug_on ? 1 : 0 }"
+  content    = "${aws_route53_record.bastionhostdnsdirekt.fqdn}"
+  filename   = "${path.module}/debug/${terraform.workspace}/bastionHost.txt"
 }
