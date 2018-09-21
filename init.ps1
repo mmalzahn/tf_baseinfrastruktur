bcbcfc605d680641f2956e273dae88e5bc386ae2
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-21 09:59:43 +0200

    update init Script

--- init.ps1
+++ init.ps1
@@ -9,5 +9,5 @@ terraform.exe workspace new prod
 terraform.exe workspace new test
 terraform.exe workspace new dev
 Set-Location -Path "$proPath\packer"
-packer.exe -var-file .\var-bastionhost.json .\bastionhost.json
+packer.exe build -var-file .\var-bastionhost.json .\bastionhost.json
 Set-Location -Path $proPath
